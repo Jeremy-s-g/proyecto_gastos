a8cfd4860dbbdb8fe458a95011c7c1352a2c50a2
Jeremy Sanhueza <jere.absalon@gmail.com>
2026-06-20 00:10:09 -0400

    Representa gastos como objetos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
 const ingresos = ["Sueldo principal", "Sueldo secundario", "Ingreso adicional"];
-const gastos = ["Arriendo", "Luz", "Agua", "Internet", "Supermercado"];
+
+const gastos = [
+  {
+    nombre: "Arriendo",
+    monto: 350000,
+    categoria: "Vivienda"
+  },
+  {
+    nombre: "Luz",
+    monto: 25000,
+    categoria: "Servicios"
+  },
+  {
+    nombre: "Agua",
+    monto: 18000,
+    categoria: "Servicios"
+  },
+  {
+    nombre: "Internet",
+    monto: 30000,
+    categoria: "Servicios"
+  },
+  {
+    nombre: "Supermercado",
+    monto: 180000,
+    categoria: "Alimentacion"
+  }
+];
+
 const deudas = ["Tarjeta de credito", "Credito de consumo", "Compra en cuotas"];
 
 console.log("Ingresos:", ingresos);
